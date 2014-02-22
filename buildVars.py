b349d0022385c897cccfb6e5676716074a6ef49d
Camlorn <camlorn38@gmail.com>
2014-02-22 12:52:34 -0500

    Bumped version to 0.1

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon-description" : _("""Adds 3D audio for controls and replaces control messages."""),
 	# version
-	"addon-version" : "dev",
+	"addon-version" : "0.1",
 	# Author(s)
 	"addon-author" : "Camlorn <camlorn38@gmail.com>, Bryan Smart",
 	# URL for the add-on documentation support
