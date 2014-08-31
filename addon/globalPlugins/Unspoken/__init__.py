dec8e7cedcb478a42122dc12a020cd877aa301c4
Austin Hicks <camlorn38@gmail.com>
2014-08-31 14:06:24 -0400

    Updated everything for Libaudioverse, including bundling mit.hrtf and enum34

--- addon/globalPlugins/Unspoken/__init__.py
+++ addon/globalPlugins/Unspoken/__init__.py
@@ -1,22 +1,30 @@
 # Unspoken user interface feedback for NVDA
-# By Bryan Smart (bryansmart@bryansmart.com)
+# By Bryan Smart (bryansmart@bryansmart.com) and Austin Hicks (camlorn38@gmail.com)
 
 import os
+import os.path
 import sys
 import globalPluginHandler
 import NVDAObjects
 import config
 import speech
 import controlTypes
-from camlorn_audio import *
 import time
 
-
-# Constants
-
-# Dimensions of the audio display
-AUDIO_WIDTH = 10.0 # Width of the audio display.
-AUDIO_DEPTH = 5.0 # Distance of listener from display.
+#this is a hack.
+#Normally, we would modify Libaudioverse to know about Unspoken and NVDA.
+#But if Windows sees a DLL is already loaded, it doesn't reload it.
+#To that end, we grab the DLLs out of the Libaudioverse directory here.
+#order is important.
+import ctypes
+file_directory = os.path.split(os.path.abspath(__file__))[0]
+libaudioverse_directory = os.path.join(file_directory, 'libaudioverse')
+dll_hack = [ctypes.cdll.LoadLibrary(os.path.join(libaudioverse_directory, 'libsndfile-1.dll'))]
+dll_hack.append(ctypes.cdll.LoadLibrary(os.path.join(libaudioverse_directory, 'libaudioverse.dll')))
+#and another hack: we import enum first so that it's in sys.modules when libaudioverse tries.
+from . import enum
+sys.modules['enum'] = enum
+import libaudioverse
 
 UNSPOKEN_ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
@@ -71,40 +79,34 @@ controlTypes.ROLE_SPLITBUTTON : "splitbutton.wav",
 
 sounds = dict() # For holding instances in RAM.
 
+#taken from Stackoverflow. Don't ask.
+def clamp(my_value, min_value, max_value):
+	return max(min(my_value, max_value), min_value)
+
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def __init__(self, *args, **kwargs):
 		globalPluginHandler.GlobalPlugin.__init__(self, *args, **kwargs)
-
-		init_camlorn_audio()
-
-		# Load sounds.
-		for key in sound_files:
-			print "Attempting to load "+sound_files[key]
-			sounds[key] = Sound3D(os.path.join(UNSPOKEN_SOUNDS_PATH, sound_files[key]))
-			sounds[key].set_rolloff_factor(0)
-			if sounds[key].get_length() <= 0:
-				print "Failed to load", key, "with length", sounds[key].get_length()
-			else:
-				print "Loaded sound "+sound_files[key]
-
-		# Setup room ambience
-		self._room_reverb = Reverb()
-		self._room_reverb.set_reverb_density(0)
-		self._room_reverb.set_Decay_time(0.4)
-		self._room_reverb.set_gain(1)
-		self._room_reverb.set_reflections_gain(0.4)
-		self._room_reverb.set_late_reverb_gain(0)
-#		for sound in sounds:
-#			sounds[sound].set_effect_for_slot(self._room_reverb, 1)
-
+		self.simulation = libaudioverse.Simulation(device_index = -1)
+		self.make_sound_objects()
+		self.hrtf_panner = libaudioverse.HrtfObject(self.simulation, os.path.join(UNSPOKEN_ROOT_PATH, 'mit.hrtf'))
+		self.simulation.output_object = self.hrtf_panner
 		# Hook to keep NVDA from announcing roles.
 		self._NVDA_getSpeechTextForProperties = speech.getSpeechTextForProperties
 		speech.getSpeechTextForProperties = self._hook_getSpeechTextForProperties
-
 		self._previous_mouse_object = None
 		self._last_played_object = None
 		self._last_played_time = 0
+		#these are in degrees.
+		self._display_width = 160.0
+		self._display_height = 40.0
+
+	def make_sound_objects(self):
+		"""Makes sound objects from libaudioverse."""
+		for key, value in sound_files.iteritems():
+			path = os.path.join(UNSPOKEN_SOUNDS_PATH, value)
+			libaudioverse_object = libaudioverse.FileObject(self.simulation, path)
+			sounds[key] = libaudioverse_object
 
 	def _hook_getSpeechTextForProperties(self, reason=NVDAObjects.controlTypes.REASON_QUERY, *args, **kwargs):
 		role = kwargs.get('role', None)
@@ -114,7 +116,6 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		return self._NVDA_getSpeechTextForProperties(reason, *args, **kwargs)
 
 	def play_object(self, obj):
-		global AUDIO_WIDTH, AUDIO_DEPTH
 		curtime = time.time()
 		if curtime-self._last_played_time < 0.1 and obj is self._last_played_object:
 			return
@@ -126,7 +127,6 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			desktop = NVDAObjects.api.getDesktopObject()
 			desktop_max_x = desktop.location[2]
 			desktop_max_y = desktop.location[3]
-			desktop_aspect = float(desktop_max_y) / float(desktop_max_x)
 			# Get location of the object.
 			if obj.location != None:
 				# Object has a location. Get its center.
@@ -137,11 +137,19 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 				obj_x = desktop_max_x / 2.0
 				obj_y = desktop_max_y / 2.0
 			# Scale object position to audio display.
-			position_x = (obj_x / desktop_max_x) * (AUDIO_WIDTH * 2) - AUDIO_WIDTH
-			position_y = (obj_y / desktop_max_y) * (desktop_aspect * AUDIO_WIDTH * 2) - (desktop_aspect * AUDIO_WIDTH)
-			position_y *= -1
-			sounds[role].set_position(position_x, position_y, AUDIO_DEPTH * -1)
-			sounds[role].play()
+			angle_x = ((obj_x-desktop_max_x/2.0)/desktop_max_x)*self._display_width
+			angle_y = (((desktop_max_y/2.0)-obj_y)/desktop_max_y)*self._display_height
+			#clamp these to Libaudioverse's internal ranges.
+			angle_x = clamp(angle_x, -90.0, 90.0)
+			angle_y = clamp(angle_y, -90.0, 90.0)
+			#Suspend the HRTFpanner.
+			self.hrtf_panner.suspended = True	
+			self.hrtf_panner.inputs[0] = sounds[role], 0
+			self.hrtf_panner.azimuth = angle_x
+			self.hrtf_panner.elevation = angle_y
+			sounds[role].position = 0
+			#Turn it back on.
+			self.hrtf_panner.suspended = False
 
 	def event_becomeNavigatorObject(self, obj, nextHandler):
 		self.play_object(obj)
